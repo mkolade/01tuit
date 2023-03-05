fab81527c0f6105b10003ba22ccae911c7ae5f15
moshoodmohammed <mohammedamoshood@gmail.com>
2023-03-06 00:32:49 +0100

    i added react icons

---
+++ src/Content.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react';
+import {FaTrashAlt} from 'react-icons/fa';
+const Content = () => {
+    const [items,setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half pound of Cocoa Covered unsalted Almonds"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
+    
+    
+    return (
+        <main>
+            <ul>
+                {items.map((item) => (
+                    <li className='item' key={item.id}>
+                        <input 
+                            type="checkbox" 
+                            checked = {item.checked}
+                        />
+                        <label>{item.item}</label>
+                        <FaTrashAlt/>
+                        
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
+
+export default Content
